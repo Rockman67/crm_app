8b80a9d13ac749b377fcecd86e45478990252cfe
Rockman67 <a.s.boltenkov.vst@gmail.com>
2024-07-21 22:07:02 +0600

    Add setup.py for project configuration

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crm_app',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # добавьте ваши зависимости сюда, например:
+        # 'flask',
+        # 'sqlalchemy',
+        # 'pytest',
+    ],
+)
