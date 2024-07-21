77203adc1c18afa5faff612c3051f96198bdbdbb
Rockman67 <130305105+Rockman67@users.noreply.github.com>
2024-07-22 01:49:25 +0600

    Update routes.py

--- auth/routes.py
+++ auth/routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from crm_app.forms import RegistrationForm, LoginForm  # Убедитесь, что формы определены правильно
+from crm_app.forms import RegistrationForm, LoginForm
 from flask_login import login_user, logout_user, login_required, current_user
+from crm_app.models import User
+from crm_app import db
 
 auth = Blueprint('auth', __name__)
+main = Blueprint('main', __name__)  # Убедитесь, что это определено
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -10,7 +13,6 @@ def login():
         return redirect(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
-        # Логика входа
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user)
@@ -30,7 +32,6 @@ def register():
         return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        # Логика регистрации
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
@@ -38,3 +39,7 @@ def register():
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('auth.login'))
     return render_template('register.html', form=form)
+
+@main.route('/')
+def home():
+    return render_template('home.html')
