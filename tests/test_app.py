545fafc409517880ab4fc21d2cdc2ec1a04b2e8c
Rockman67 <130305105+Rockman67@users.noreply.github.com>
2024-07-22 00:13:05 +0600

    Create test_app.py
    Add test_app.py for basic tests

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+import unittest
+from crm_app.app import app  # Убедитесь, что путь правильный
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
