93b2a02206dd6a6d5d061f781e9952394e3c22fa
Rockman67 <130305105+Rockman67@users.noreply.github.com>
2024-07-22 01:39:24 +0600

    Update routes.py

--- auth/routes.py
+++ auth/routes.py
@@ -1,16 +1,40 @@
-from flask import Blueprint, render_template, redirect, url_for
-from crm_app.forms import RegistrationForm  # Убедитесь, что путь правильный
+from flask import Blueprint, render_template, redirect, url_for, flash
+from crm_app.forms import RegistrationForm, LoginForm  # Убедитесь, что формы определены правильно
+from flask_login import login_user, logout_user, login_required, current_user
 
-main = Blueprint('main', __name__)
+auth = Blueprint('auth', __name__)
 
-@main.route('/')
-def home():
-    return render_template('home.html')
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Логика входа
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
 
-@main.route('/register', methods=['GET', 'POST'])
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         # Логика регистрации
-        return redirect(url_for('main.home'))
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('auth.login'))
     return render_template('register.html', form=form)
