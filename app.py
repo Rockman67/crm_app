2be58d61284169a7773c4cb9e10565909987cc76
Rockman67 <130305105+Rockman67@users.noreply.github.com>
2024-07-22 01:15:53 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from crm_app.config import Config
-from crm_app.routes import main
+from crm_app.auth.routes import main
 
 app = Flask(__name__)
 app.config.from_object(Config)
